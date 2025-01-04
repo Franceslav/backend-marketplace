98e869df153ac48f51e6d855f417b8f5def43d70
Franz <f.o.larionov@gmail.com>
2025-01-04 18:49:47 +0300

    fix for render

--- app/config.py
+++ app/config.py
@@ -1,7 +1,5 @@
 from typing import Literal
-
 from pydantic_settings import BaseSettings
-
 
 class Settings(BaseSettings):
     MODE: Literal["DEV", "TEST", "PROD"]
@@ -13,11 +11,9 @@ class Settings(BaseSettings):
     DB_PASS: str
     DB_NAME: str
 
-
-    
     @property
     def DATABASE_URL(self):
-        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}?sslmode=require"
     
     TEST_DB_HOST: str
     TEST_DB_PORT: int
@@ -25,14 +21,9 @@ class Settings(BaseSettings):
     TEST_DB_PASS: str
     TEST_DB_NAME: str
 
-    # @root_validator
-    # def get_test_database_url(cls, v):
-    #     v["TEST_DATABASE_URL"] = f"postgresql+asyncpg://{v['TEST_DB_USER']}:{v['TEST_DB_PASS']}@{v['TEST_DB_HOST']}:{v['TEST_DB_PORT']}/{v['TEST_DB_NAME']}"
-    #     return v
-   
     @property
     def TEST_DATABASE_URL(self):
-        return f"postgresql+asyncpg://{self.TEST_DB_USER}:{self.TEST_DB_PASS}@{self.TEST_DB_HOST}:{self.TEST_DB_PORT}/{self.TEST_DB_NAME}"
+        return f"postgresql+asyncpg://{self.TEST_DB_USER}:{self.TEST_DB_PASS}@{self.TEST_DB_HOST}:{self.TEST_DB_PORT}/{self.TEST_DB_NAME}?sslmode=require"
     
     SMTP_HOST: str
     SMTP_PORT: int
@@ -48,6 +39,8 @@ class Settings(BaseSettings):
     ALGORITHM: str
 
     class Config:
-        env_file = ".env"
+        # Удалите или закомментируйте следующую строку
+        # env_file = ".env"
+        pass
 
 settings = Settings()
